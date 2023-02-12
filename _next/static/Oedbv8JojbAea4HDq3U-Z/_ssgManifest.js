16e698245dc6c94f230d14e2b02371d65c9f808b
Kushan Janith <kushan.janith@ivedha.com>
2023-02-13 00:30:48 +0530

    initial

---
+++ _next/static/Oedbv8JojbAea4HDq3U-Z/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fworks","\u002Fcontacts","\u002Fexhibitions","\u002F","\u002Fmovies","\u002Fbiography","\u002Fexhibitions\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
